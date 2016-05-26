eeb834ac0c2d1eab01cbf6472715c71bd041aeeb
mpanowicz <michal.panowicz@student.put.poznan.pl>
2016-05-26 14:52:41 +0200

    Server sending amount of corpses

---
+++ CorpseServer/corpseServer.cpp
@@ -0,0 +1,57 @@
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <string.h>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <fstream>
+
+#define readI(fd, x) read(fd, x, sizeof(int))
+#define readF(fd, x) read(fd, x, sizeof(float))
+#define writeI(fd, x) write(fd, x, sizeof(int))
+#define writeF(fd, x) write(fd, x, sizeof(float))
+
+using namespace std;
+
+void childend(int signum)
+{
+	wait(NULL);
+	printf("Koniec połączenia\n");
+}
+
+int main(int argc, char** argv) {
+    unsigned int fdc, size, on=1; //client
+    struct sockaddr_in lsa; //server
+    struct sockaddr_in laddr; //client
+	if(signal(SIGCHLD, childend) == SIG_IGN)
+		signal(SIGCHLD, SIG_IGN);
+	
+	fdc = socket(PF_INET, SOCK_STREAM, 0);
+	lsa.sin_family = PF_INET;
+	lsa.sin_port = htons(1234);
+	lsa.sin_addr.s_addr = INADDR_ANY;
+	bind(fdc, (struct sockaddr*)&lsa, sizeof(lsa));
+	listen(fdc, 10);
+	
+	size=sizeof(laddr);
+
+	int fd2;
+	while(1){
+		fd2 = accept(fdc, (struct sockaddr*)&laddr, &size);
+		if(!fork()){
+			close(fdc);
+			int count = 1;
+			printf("Nowe połączenie: %s:%i\n", inet_ntoa((struct in_addr)laddr.sin_addr), laddr.sin_port);
+			writeI(fd2, &count);
+			close(fd2);
+			return 0;
+		}
+	}
+    return 0;
+}
