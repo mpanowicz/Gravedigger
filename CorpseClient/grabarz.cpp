3f374162063e9c4291bd67ed8c876a9bb08eed59
mpanowicz <panowicz.michal@gmail.com>
2016-06-07 11:09:59 +0200

    Clear code

--- CorpseClient/grabarz.cpp
+++ CorpseClient/grabarz.cpp
@@ -11,11 +11,8 @@
 #define MAX_SIZE 256
 #define MSG_TAG 100
 
-
 #define readI(fd, x) read(fd, x, sizeof(int))
-#define readF(fd, x) read(fd, x, sizeof(float))
 #define writeI(fd, x) write(fd, x, sizeof(int))
-#define writeF(fd, x) write(fd, x, sizeof(float))
 
 
 using namespace std;
@@ -40,13 +37,11 @@ int getPosition(int rank){
         if(rank == processList[i].first )
             return i;
     }
-    //return processList.size()-1;
     return -1;
 }
 
 void printProcessList(int rank, vector < pair <int, long int> > processList){
     int i = 0;
-    //if(processList.size() > 3)
     for(pair<int, long int>& process : processList) {
         printf("%d: process[%d]: %d, %ld\n", rank, i, process.first, process.second);        
         i++;
@@ -55,22 +50,14 @@ void printProcessList(int rank, vector < pair <int, long int> > processList){
 
 void sortProcessList(int rank){
     /*     BUBBLE SORT XD    */
-    //cout << rank << " : SORT PRZED" << endl;
-    //printProcessList(rank, processList);
     pair <int, long int> tmp;
     for(unsigned int j = 0; j < processList.size() - 1; j++){
         for(unsigned int i = 0; i < processList.size() - 1; i++){            
             if(processList[i].second > processList[i+1].second 
-                && processList[i+1].second > 0) {  
-                //if(rank == 1) printf("PRZED: i %d j %d\np[i].1: %d, p[i+1].1 %d\np[i].2  %ld p[i+1].2 %ld\n", i,j, processList[i].first, processList[i+1].first, processList[i].second, processList[i+1].second);
+                && processList[i+1].second > 0) { 
                 tmp = processList[i+1]; 
-                //if(rank == 1 ) printf("TMP first: %d, second: %ld\n",  tmp.first, tmp.second);
-                         
                 processList[i+1] = processList[i];
-                
                 processList[i] = tmp; 
-                //if(rank == 1) printf("i first: %d, second: %ld\n", processList[i].first, processList[i].second); 
-                //if(rank == 1) printf("PO\np[i].1: %d, p[i+1].1 %d\np[i].2  %ld p[i+1].2 %ld\n",  processList[i].first, processList[i+1].first, processList[i].second, processList[i+1].second);
             }
         }          
     }    
@@ -79,31 +66,17 @@ void sortProcessList(int rank){
         for(unsigned int i = 0; i < processList.size() - 1; i++){
             if(processList[i].second == processList[i+1].second && 
                 processList[i].first > processList[i+1].first ) {
-                //if(rank == 1) printf("%d %d\n", i,j);
-                //if(rank == 1) printf("PRZED: i %d j %d\np[i].1: %d, p[i+1].1 %d\np[i].2  %ld p[i+1].2 %ld\n", i,j, processList[i].first, processList[i+1].first, processList[i].second, processList[i+1].second);
-                
                 tmp = processList[i+1]; 
-                //if(rank == 1 ) printf("TMP first: %d, second: %ld\n",  tmp.first, tmp.second);
-                         
-                processList[i+1] = processList[i];
-                //if(rank == 1) printf("i+1 first: %d, second: %ld\n", processList[i+1].first, processList[i+1].second);
-                processList[i] = tmp; 
-                //if(rank == 1) printf("i first: %d, second: %ld\n", processList[i].first, processList[i].second);  
-                //if(rank == 1) printf("PO\np[i].1: %d, p[i+1].1 %d\np[i].2  %ld p[i+1].2 %ld\n",  processList[i].first, processList[i+1].first, processList[i].second, processList[i+1].second);
+                processList[i+1] = processList[i];                
+                processList[i] = tmp;
             }
         }          
     }        
-   
-   cout << rank << " : SORT PO" << endl;
-   printProcessList(rank, processList);
-   
 }
 
 bool canITakeCorpse(int size, int rank, int corpse){
     sortProcessList(rank);
-    int diff = 0;//size - processList.size(); // jak nie mamy odpowiedzi od wszystkich to zakladamy, że
-    cout << rank << ": position: " << getPosition(rank) << " corpse: " << corpse << endl;
-    if(getPosition(rank) + diff < corpse) // ten, którego zegara nie znamy jest przed nami
+    if(getPosition(rank) < corpse) 
         return true;
     else 
         return false;    
@@ -115,149 +88,57 @@ long int getNewPriority(){
     return clock;
 }
 
-int sendRelease(int size, long int rank){
-    /*
-    //long int msg[3];
-    //msg[0] = rank;
-    //msg[1] = -1;
-    //msg[2] = 0;
-    for(int i = 0; i < size; i++){
-        if(i != rank){
-            //wyślij priorytet
-            //MPI_Send( msg, 3, MPI_LONG_INT, i, MSG_TAG, MPI_COMM_WORLD );
-            //printf(" %ld: Wyslalem release: %ld, priority: %ld, type: %ld do %d\n", rank, msg[0], msg[1], msg[2], i );
-        }
-    }*/
-    
-    int corpse_taken = getPosition(rank)+1; //CHYBA +1
-    cout << "RELEASE PRZED:" << endl;
-    printProcessList(rank, processList);
+int sendRelease(int size, long int rank, long priority){
+        
+    int corpse_taken = getPosition(rank)+1; 
+    cout << priority << " " << rank << ": rozsyłam release " << endl;  
     processList.erase (processList.begin(), processList.begin() + corpse_taken);
-    corpse -= corpse_taken;
-    cout << rank << " : corpse -= corpse_taken = " << corpse << endl;
-    cout << "RELEASE PO:" << endl;
-    printProcessList(rank, processList);
-    
+    corpse -= corpse_taken;    
     return size - corpse_taken; // = msg_count 
 }
 
-void receiveMessages(int msg_count, int size, int rank, long int *msg, bool flag){
-    MPI_Status status;
-    
-    //int additionalCounter = 0;
-    while(msg_count != size - 1){
-        cout << rank << " : recv msg 1" << msg_count << endl;
-        //if(additionalCounter == size)
-        //    break;
-        
-        cout << rank << " : recv msg 2" << msg_count << endl;
-        if( MPI_Recv(msg, 3, MPI_LONG_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status) != 0){
-            cout << "\t\t\t\tBREAK!!****" << endl;
-            break;
-        }
-        //int recv = MPI_Recv(msg, 3, MPI_LONG_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-        //cout << rank << " MPI_RECV count: "/* << status.count*/ << " recv: " << recv << endl;
-        cout << rank << " : recv msg 3" << msg_count << endl;
-        printf(" %d: Otrzymalem rank: %ld, priority: %ld, type: %ld od %d\n", rank, msg[0], msg[1], msg[2], status.MPI_SOURCE);
-        
-        //sleep(2);   
-        /*
-        if(msg[2] == 0){ //release
-            corpse--;   
-            //msg_count--;
-            printf("\t%d: usuwam proces nr ", rank);
-            cout << getPosition(msg[0]) << endl;
-            
-            
-            //cout << rank <<  " : LISTA PRZED RCV RLS" << endl;
-            //printProcessList(rank, processList);
-            if(getPosition(msg[0]) == -1){
-                cout << rank << " : nie znalazlem!" << endl;
-            } else {
-                processList.erase(processList.begin()+getPosition(msg[0])); 
-                cout << rank << " : usunalem" << getPosition(msg[0]) << endl;
-                
-            }
-            
-            cout << rank <<  " : LISTA PO RCV RLS" << endl;
-            printProcessList(rank, processList);
-            
-            //usun z listy proces który wykonał pogrzeb
-        } else */
+void receiveMessages(int msg_count, int size, int rank, long int *msg, bool flag){    
+    MPI_Status status;    
+    while(msg_count != size - 1){                
+        MPI_Recv(msg, 3, MPI_LONG_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);         
         if (msg[2] == 1){ //request        
             msg_count++;
             if(getPosition(msg[0]) < 0)
-                addToProcessList(msg[0], msg[1]);  
-            cout << rank << " : dodaje:" << msg[0] << " " << msg[1] << endl;
-        /*
-        //printf("%d : LISTA W RCV 1\n", rank);
-        //printProcessList(rank, processList);
-        
-            if (canITakeCorpse(size, rank, corpse)){
-                printf("%d: BIORE GO SZYBCIEJ!\n", rank);
-                //goto potrzeb
-                //printf("BIORE GO !\n");
-                
-                
-                return true;  //jak 1 to jump do startu
-                
-            }*/            
-        } else { // msg[2] == 2, czyli batch
-            //corpse += (int)msg[1];
-            cout << "ELSE " << rank << ": " << corpse << endl;
+                addToProcessList(msg[0], msg[1]);        
         }
-        //if(flag)
-        //    additionalCounter++;
     }   
-    cout << rank << " : WYCHODZE Z RECV" << endl;
 }
 
-int pogrzeb(int size, int rank){
-    
-    cout << " " << rank << " :PRZEBIEGA POGRZEB JEDNEGO Z TRUPÓW...\n";
-    //corpse--;      
-    sleep(5);    
-    //cout << rank << "KONIEC POGRZEBU...\n";
-    int new_msg_count = sendRelease(size, rank); 
-    
-    cout << rank << "KONIEC POGRZEBU...\n";
-    
-    printf("%d corpse: %d\n", rank, corpse);
-    //receiveMessages(6,  size,  rank, msg, 1);
+int pogrzeb(int size, int rank, long priority){       
+       
+    int new_msg_count = sendRelease(size, rank, priority);  
+    cout << priority << " " << rank << ": WYKONUJE POGRZEB " << endl;
+    sleep(5); 
     return new_msg_count;
 }
-
 
 pair<int, int> askForCorpseNum(int rank, int current_corpses){
 
     pair<int, int> corpses_pair;
-    
-    //printf("askForCorpseNum:%d Time:%ld\n", rank);
     int fd, con = 0;
     struct sockaddr_in sa;
     struct hostent* addrent;
     fd = socket(PF_INET, SOCK_STREAM, 0);
-    //addrent=gethostbyname("panowiczmichal.ddns.net");
-    addrent=gethostbyname("localhost");
+    addrent=gethostbyname("lab-os-6");
     sa.sin_family = PF_INET;
     sa.sin_port = htons(8080);
     memcpy(&sa.sin_addr.s_addr, addrent->h_addr, addrent->h_length);
-    //cout << "askForCorpseNum1 " << con << endl;
     con = connect(fd, (struct sockaddr*)&sa, sizeof(sa));
-    //cout << "askForCorpseNum2 " << con << endl;
     int corpses = 0;
     if(con == 0){
         int count;
         writeI(fd, &rank);
         readI(fd, &count);
-        //read(fd, &time, sizeof(long));
         if(count > current_corpses){
             corpses = count - current_corpses;
             current_corpses = count;
-            printf("%d: Corpses:%d\n", rank, count);
         }
         close(fd);
-        //break;
     }
     close(fd);
     corpses_pair.first = corpses;
@@ -272,158 +153,77 @@ int main(int argc, char **argv) {
     int size, rank, len;
 	char processor[100];
     long int priority = 0;
-    //int corpse = 2;
 	MPI_Init(&argc,&argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank); // ktory watek
     MPI_Comm_size(MPI_COMM_WORLD, &size); // ile watkow
 	MPI_Get_processor_name(processor, &len);
-    
+    printf("Hello world: %d of %d na (%s)\n", rank, size, processor);
     int current_corpses = 0;
-    bool start = true;    
-    int msg_count = 0;
-        
+    int msg_count = 0;        
     
     while(1){
     /*  START   */ 
-
         long int msg[3];        
         int type = 1; 
         
-    
-        if(!start){
-            cout << rank << " : MSG COUNT = " << msg_count << endl; 
-        }
-        
         if(getPosition(rank) < 0){ //nie ma mnie na liście
             priority = getNewPriority();
-            printf("%d: my priority: %ld \n", rank, priority);
             addToProcessList(rank, priority);   
+            cout << priority << " " << rank << ": pobieram wartość zegara " << priority << endl;
 
             msg[0] = (long)rank;
             msg[1] = priority; 
             msg[2] = type;  
             //rozgłaszanie
+            
+            cout << priority << " " << rank << ": rozsyłam priorytety" << endl;
             for(int i = 0; i < size; i++){
                 if(i != rank){
-                    //wyślij priorytet
-                    MPI_Send( msg, 3, MPI_LONG_INT, i, MSG_TAG, MPI_COMM_WORLD );
-                    printf("%d: Wyslalem rank: %ld, priority: %ld, type: %ld do %d\n", rank, msg[0], msg[1], msg[2], i );
+                    MPI_Send( msg, 3, MPI_LONG_INT, i, MSG_TAG, MPI_COMM_WORLD );                    
                 }
-            }
-            
-        }
+            }            
+        }        
         
-        
-        printProcessList(rank, processList);
-        
-        
-        
-        
-              
-        
+        cout << priority << " " << rank << ": czekam na trupy " << endl;
         while(corpse < 1){
-            printf("%d : czekam na pojawienie sie nowych trupów...\n", rank);
             pair<int, int> corpses_pair;
+            
+            
+            
             corpses_pair = askForCorpseNum(rank, current_corpses);
             corpse = corpses_pair.first;
-            current_corpses = corpses_pair.second;
-            cout << rank << " : corpse: " << corpse << endl;
-            
+            current_corpses = corpses_pair.second;         
         }
-        
-        
-        /*
-        - wysylam wszystkim swoj priorytet
-        - odbieram od wszystkich priorytety
-        - dodaje ich do listy
-        */
-        
-        
-        
-        /*
-            0 - release
-            1 - request
-            2 - batch
-        */
-        
-        
-        
-        
-        
-        
-        
-        //odbieranie
-        int new_rank = rank;        
-        cout << rank << " : GOTCORPSE1: " << endl;
-        receiveMessages(msg_count, size, rank, msg, 0); 
-        printf("%d %d GOTCORPSE2\n", rank, new_rank);
-        rank = new_rank;
-        
-        //zeruje count
-        /*
-        if(gotCorpse){            
-            //cout << rank << " : LISTA PRZED" << endl;
-            //printProcessList(rank, processList);      
-            printf("GOTCORPSE: %d\n", rank);
-            pogrzeb(size, rank);
-            receiveMessages(msg_count, size, rank, msg, 0);  
-            cout << rank << " : LISTA PO" << endl;
-            printProcessList(rank, processList);
-            
-            continue;
-        }*/
-        
-            //odbierz msg
-            
-        
-        
-        //if(rank == 1)
+        cout << priority << " " << rank << ": doczekałem się " << corpse << " trupów " << endl;
         //printProcessList(rank, processList);
+        //odbieranie
+		//cout << rank << " rank B4 msg_count: " << msg_count << endl;
+        int new_rank = rank;
+        cout << priority << " " << rank << ": odbieram priorytety" << endl;
+        receiveMessages(msg_count, size, rank, msg, 0); 
+        rank = new_rank;
+		//cout << rank << " rank AFTER" << new_rank << endl;
         
-        printf("%d WYSZEDLEM Z RECEIVE, count: %d\n", rank, msg_count);
+        cout << priority << " " << rank << ": sprawdzam czy moge brac trupa" << endl;
         if(canITakeCorpse(size, rank, corpse)){
-            printf("%d : BIORE GO !\n", rank);
-            //cout << rank << " : LISTA PRZED" << endl;
-            //printProcessList(rank, processList);
-            //printf("CANITAKECORPSE: %d\n", rank);
-            msg_count = pogrzeb(size, rank);
-            cout << rank << " : new_msg_count" << msg_count << endl;
-            //receiveMessages(msg_count, size, rank, msg, 0); 
-            //cout << rank << " : LISTA PO" << endl;
-            //printProcessList(rank, processList);
-            //
-            
-            //cout << "LISTA PO" << endl;
-            //printProcessList(rank, processList);
+            cout << priority << " " << rank << ": wiem, że moge brać trupa" << endl;
+            msg_count = pogrzeb(size, rank, priority);
         } else {
-            printf("%d MUSZE CZEKAC :(\n", rank);
-            //czekaj na nowe wiadomosci
+			cout << priority << " " << rank << ": wiem, że NIE moge brać trupa" << endl;
             msg_count = size - 1 - corpse;
             processList.erase (processList.begin(), processList.begin() + corpse);
-            cout << rank << " : musze_czekac msg_count" << msg_count << endl;
-            printProcessList(rank, processList);
             corpse = 0;            
-            //receiveMessages(msg_count, size, rank, msg, 0);  
         }
-        
-        //printf("My ID: %d, priority: %ld, corpses: %d\n", rank, priority, corpse); 
-        //printProcessList(rank, processList);
-        
-        
-        //sleep(5);
-        
-        //processList.erase(processList.begin()+1);
-        start = false;
     }
-	cout << "koniec pracy procesu " << rank << endl;
+	cout << priority << " " << ": koniec pracy procesu " << rank << endl;
 	MPI_Finalize();
 }
 
 
 /*
-
-mpiCC grabarz.cpp  -std=c++11 -o g.exe
-mpirun -default-hostfile none -np 5 ./g.exe localhost
-mpirun -default-hostfile none -np 5 ./a.out 
-
+mpirun -default-hostfile none -np 10 ./g.exe
+mpirun -default-hostfile none -hostfile hosty -np 10 ./g.exe > plik
+mpirun -default-hostfile none -hostfile hosty -np 10 ./g.exe
+mpiCC grabarz.cpp -Wall  -std=c++11 -o g.exe
+g++ serv.cpp -o serv
 */
